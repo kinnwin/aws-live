e9303d80eb151c9c6a59dd8abe5005e466e8166a
kinnwin <110600890+kinnwin@users.noreply.github.com>
2024-04-16 15:03:42 +0800

    Add files via upload

--- aws-live-main/config.py
+++ aws-live-main/config.py
@@ -1,7 +1,7 @@
 customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "Aacs3253"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "chinkinnwynn-employee"
 customregion = "us-east-1"
 
